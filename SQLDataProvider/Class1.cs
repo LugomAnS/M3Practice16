4ec93403adb4167a5a89db72f2383a75ed37ba64
Anton Lugom <mogul086@mail.ru>
2023-02-15 21:11:49 +0300

    SQL DB

---
+++ SQLDataProvider/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DataProvider
+{
+    public class SQLConnectionDB
+    {
+        private SqlConnectionStringBuilder connString;
+
+        public SQLConnectionDB()
+        {
+            connString = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\mssqllocaldb",
+                InitialCatalog = "ADONETTestDB",
+                IntegratedSecurity = true
+            };
+        }
+
+        public string OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection() { ConnectionString = connString.ConnectionString };
+
+            try
+            {
+                connection.Open();
+                return "Соединение установлено";
+            }
+            catch (Exception)
+            {
+                return "Исключение";
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
