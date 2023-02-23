0188f89a3d52d73f51fbfcb05d5cd9a070f3cab4
Anton Lugom <mogul086@mail.ru>
2023-02-23 12:36:37 +0300

    Создание таблицы и заполнение данными

--- AdoNet/SQLScripts/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[ID] INT NOT NULL IDENTITY, 
-    [Surname] NCHAR(15) NOT NULL, 
-    [Name] NCHAR(15) NOT NULL, 
-    [Patronymic] NCHAR(15) NOT NULL, 
-    [Phone] NCHAR(15) NULL, 
-    [Email] NVARCHAR(50) NOT NULL 
-)
